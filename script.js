1089a046d6db9496760a6b1d842899dd22763f0c
enrichdev-en <75846773+enrichdev-en@users.noreply.github.com>
2021-01-14 21:55:13 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,110 @@
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
+const dateEl = document.querySelector(".date");
+const html = document.querySelector("html");
+const toggle = document.querySelector(".toggle");
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+const autoDarkMode = () => {
+  const time = new Date();
+  const hour = time.getHours();
+  
+  if (hour >= 18 || hour < 6) {
+    html.classList.add("dark");
+    toggle.innerHTML = "Light mode";
+  }
+};
+
+autoDarkMode();
+
+// Toggle between Light and DarkMode
+toggle.addEventListener("click", e => {
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light mode";
+  }
+});
+
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hours = time.getHours();
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+
+  const displayTime = `${hoursForClock}:${
+    minutes > 10 ? minutes : "0" + minutes
+  }`;
+
+  hours < 12
+    ? (timeEl.innerHTML = displayTime + " AM")
+    : (timeEl.innerHTML = displayTime + " PM");
+
+  dateEl.innerHTML = `${days[day]},  ${months[month]} <span class='circle'>${date}</span>`;
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+// setInterval( () => setTime(), 1000)
+setInterval(setTime, 1000);
+
+// setTime();
